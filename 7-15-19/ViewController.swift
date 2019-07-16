c7cb55261838ebb621a7cf1b15fcbe6559b08272
debesh <debeshdassharma@gmail.com>
2019-07-16 09:17:06 -0700

    Initial Commit

---
+++ 7-15-19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  7-15-19
+//
+//  Created by Spur IQ on 7/16/19.
+//  Copyright © 2019 Ricky Cui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
