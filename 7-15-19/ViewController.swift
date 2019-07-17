35f9ca2ccfac984923d6d86507dd960cabac497f
Ricky Cui <cuiricky4@gmail.com>
2019-07-17 12:55:06 -0700

    Dice

--- 7-15-19/ViewController.swift
+++ 7-15-19/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var diceImages:[String]=["Dice Small Blue 1","Dice Small Blue 2","Dice Small Blue 3","Dice Small Blue 4","Dice Small Blue 5","Dice Small Blue 6"]
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        imageView.image=UIImage(named:"Spur IQ Icon Logo")
     }
 
-
+    @IBAction func button1(_ sender: Any) {
+        imageView.image=UIImage(named: diceImages[Int.random(in:1...6)])
+    }
+    
 }
 
